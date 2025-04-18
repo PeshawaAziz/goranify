cecf7496c6ce5fea87406f7b59c3f3cd7c5ca3e8
PeshawaAziz <modern3p2017@gmail.com>
2025-04-18 21:29:48 +0330

    feat: implement Music class

--- core/Music.java
+++ core/Music.java
@@ -1,5 +1,82 @@
 package core;
 
-public class Music {
+import java.util.ArrayList;
 
+public class Music {
+    private static ArrayList<Music> allMusics = new ArrayList<>();
+
+    private String title;
+    private User singer;
+    private int numberOfStream = 0;
+
+    public Music(String title, User singer) throws IllegalArgumentException {
+        setTitle(title);
+        setSinger(singer);
+
+        allMusics.add(this);
+    }
+
+    public void play() {
+        System.out.println("Playing " + title + " by " + singer.getUsername());
+        numberOfStream++;
+    }
+
+    public ArrayList<Music> search(String title) {
+        ArrayList<Music> result = new ArrayList<>();
+        for (Music music : allMusics) {
+            if (music.getTitle().toLowerCase().contains(title.toLowerCase())) {
+                result.add(music);
+            }
+        }
+
+        return result;
+    }
+
+    public ArrayList<Music> search(String title, String singerName) {
+        ArrayList<Music> result = new ArrayList<>();
+        for (Music music : allMusics) {
+            if (music.getTitle().toLowerCase().contains(title.toLowerCase()) &&
+                    music.getSinger().getUsername().toLowerCase().contains(singerName.toLowerCase())) {
+                result.add(music);
+            }
+        }
+
+        return result;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public final void setTitle(String title) {
+        if (title == null || title.trim().isEmpty()) {
+            throw new IllegalArgumentException("Title cannot be null or empty.");
+        }
+
+        this.title = title;
+    }
+
+    public User getSinger() {
+        return singer;
+    }
+
+    public final void setSinger(User singer) {
+        if (singer == null) {
+            throw new IllegalArgumentException("Singer cannot be null.");
+        }
+
+        this.singer = singer;
+    }
+
+    public int getNumberOfStream() {
+        return numberOfStream;
+    }
+
+    public void setNumberOfStream(int numberOfStream) {
+        if (numberOfStream < 0) {
+            throw new IllegalArgumentException("Number of streams cannot be negative.");
+        }
+
+        this.numberOfStream = numberOfStream;
+    }
 }
